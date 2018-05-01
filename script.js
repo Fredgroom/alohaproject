e6388ff2196f4588ed0391a11f1dd30f5a56a283
Freddie Groom <fredgroom@gmail.com>
2018-05-01 14:36:06 +0100

    accessiblity init

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+  $(function() {
+    $("a[href^='#']").not("a[href='#']").click(function() {
+       $("#"+$(this).attr("href").slice(1)+"").focus();
+    });
+ });
+
 $('.main-carousel').flickity({
     // options
     cellAlign: 'left',
     contain: true
   });
+
